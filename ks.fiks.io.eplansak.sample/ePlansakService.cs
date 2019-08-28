8941660bb51aae51ada2ad60277f1d27a999a8ac
Tor Kjetil Nilsen <tor@arkitektum.no>
2019-08-28 12:41:03 +0200

    manage replay, more samples

---
+++ ks.fiks.io.eplansak.sample/ePlansakService.cs
@@ -0,0 +1,149 @@
+﻿using KS.Fiks.IO.Client;
+using KS.Fiks.IO.Client.Models;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Security.Cryptography.X509Certificates;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using KS.Fiks.IO.Client.Configuration;
+using Ks.Fiks.Maskinporten.Client;
+
+namespace ks.fiks.io.eplansak.sample
+{
+    public class ePlansakService : IHostedService, IDisposable
+    {
+        FiksIOClient client;
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+
+            Console.WriteLine("ePlansak Service is starting.");
+            IConfiguration config = new ConfigurationBuilder()
+
+            .AddJsonFile("appsettings.json", true, true)
+            .AddJsonFile("appsettings.development.json", true, true)
+            .Build();
+
+            Console.WriteLine("Setter opp FIKS integrasjon for ePlansak...");
+            Guid accountId = Guid.Parse(config["accountId"]);  /* Fiks IO accountId as Guid Banke kommune digitalt planregister konto*/
+            string privateKey = File.ReadAllText("privkey.pem"); ; /* Private key for offentlig nøkkel supplied to Fiks IO account */
+            Guid integrationId = Guid.Parse(config["integrationId"]); /* Integration id as Guid ePlansak system X */
+            string integrationPassword = config["integrationPassword"];  /* Integration password */
+            Guid sendToaccountId = Guid.Parse(config["sendToAccountId"]);
+
+            // Fiks IO account configuration
+            var account = new KontoConfiguration(
+                                accountId,
+                                privateKey);
+
+            // Id and password for integration associated to the Fiks IO account.
+            var integration = new IntegrasjonConfiguration(
+                                    integrationId,
+                                    integrationPassword, "ks:fiks");
+
+            // ID-porten machine to machine configuration
+            var maskinporten = new MaskinportenClientConfiguration(
+                audience: @"https://oidc-ver2.difi.no/idporten-oidc-provider/", // ID-porten audience path
+                tokenEndpoint: @"https://oidc-ver2.difi.no/idporten-oidc-provider/token", // ID-porten token path
+                issuer: @"arkitektum_test",  // issuer name
+                numberOfSecondsLeftBeforeExpire: 10, // The token will be refreshed 10 seconds before it expires
+                certificate: GetCertificate(config["ThumbprintIdPortenVirksomhetssertifikat"]));
+
+            // Optional: Use custom api host (i.e. for connecting to test api)
+            var api = new ApiConfiguration(
+                            scheme: "https",
+                            host: "api.fiks.test.ks.no",
+                            port: 443);
+
+            // Optional: Use custom amqp host (i.e. for connection to test queue)
+            var amqp = new AmqpConfiguration(
+                            host: "io.fiks.test.ks.no",
+                            port: 5671);
+
+            // Combine all configurations
+            var configuration = new FiksIOConfiguration(account, integration, maskinporten, api, amqp);
+            client = new FiksIOClient(configuration); // See setup of configuration below
+
+            Guid receiverId = sendToaccountId; // Receiver id as Guid
+            Guid senderId = accountId; // Sender id as Guid
+
+            client.NewSubscription(OnReceivedMelding);
+
+            Console.WriteLine("Abonnerer på meldinger på konto " + accountId.ToString() + " ...");
+
+            var messageRequestOpprettePlan = new MeldingRequest(
+                                       mottakerKontoId: receiverId,
+                                       avsenderKontoId: senderId,
+                                       meldingType: "no.ks.geointegrasjon.plan.oppretteplanidentinput.v1"); // Message type as string https://fiks.ks.no/plan.oppretteplanidentinput.v1.schema.json
+                                                                                                            //Se oversikt over meldingstyper på https://github.com/ks-no/fiks-io-meldingstype-katalog/tree/test/schema
+
+
+            string payload = File.ReadAllText("sampleOpprettePlan.json");
+            // Sending a string
+            Console.WriteLine("Sender melding...");
+
+            var msg = client.Send(messageRequestOpprettePlan, payload, "meldingOmNyPlan.json").Result;
+            Console.WriteLine("Melding " + msg.MeldingId.ToString() + " sendt..." + msg.MeldingType);
+            Console.WriteLine(payload);
+
+            return Task.CompletedTask;
+        }
+
+
+            static void OnReceivedMelding(object sender, MottattMeldingArgs fileArgs)
+        {
+            //Se oversikt over meldingstyper på https://github.com/ks-no/fiks-io-meldingstype-katalog/tree/test/schema
+
+            // Process the message
+
+            if (fileArgs.Melding.MeldingType == "no.ks.geointegrasjon.plan.planidentopprettet.v1")
+            {
+                Console.WriteLine("Melding " + fileArgs.Melding.MeldingId + " " + fileArgs.Melding.MeldingType + " mottas...");
+
+                //TODO håndtere meldingen med ønsket funksjonalitet
+
+                Console.WriteLine("ePlansak oppdaterer sak med tiltdelt arealplanident");
+
+                fileArgs.SvarSender.Ack(); // Ack message to remove it from the queue
+            }
+            else
+            {
+                Console.WriteLine("Ubehandlet melding i køen " + fileArgs.Melding.MeldingId + " " + fileArgs.Melding.MeldingType);
+
+            }
+        }
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine("ePlansak Service is stopping.2");
+            //Client?
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            //Client?
+            client.Dispose();
+        }
+
+        private static X509Certificate2 GetCertificate(string ThumbprintIdPortenVirksomhetssertifikat)
+        {
+
+            //Det samme virksomhetssertifikat som er registrert hos ID-porten
+            X509Store store = new X509Store(StoreLocation.CurrentUser);
+            X509Certificate2 cer = null;
+            store.Open(OpenFlags.ReadOnly);
+            //Henter Arkitektum sitt virksomhetssertifikat
+            X509Certificate2Collection cers = store.Certificates.Find(X509FindType.FindByThumbprint, ThumbprintIdPortenVirksomhetssertifikat, false);
+            if (cers.Count > 0)
+            {
+                cer = cers[0];
+            };
+            store.Close();
+
+            return cer;
+        }
+    }
+}
